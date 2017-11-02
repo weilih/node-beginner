99f8b8b3241de876e3d791c84654e1aed89dc392
willieLOH <willie.loh@live.com>
2017-11-03 01:20:59 +0800

    use 'fs' to /show image from /tmp
    npm install formidable

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,4 +1,5 @@
-var querystring = require("querystring");
+var querystring = require("querystring"),
+    fs = require("fs");
 
 function start(response, postData){
   console.log("Request handler 'start' was called.");
@@ -28,5 +29,12 @@ function upload(response, postData){
   response.end();
 }
 
+function show(response){
+  console.log("Request handler 'show' was called.");
+  response.writeHead(200, {"Content-Type": "image/png"});
+  fs.createReadStream("/tmp/test.png").pipe(response);
+}
+
 exports.start = start;
 exports.upload = upload;
+exports.show = show;
