9709361f766592e973448e7ff0db06edd832086b
willieLOH <willie.loh@live.com>
2017-11-03 00:47:16 +0800

    handling post on form

--- server.js
+++ server.js
@@ -3,10 +3,20 @@ var url = require("url");
 
 function start(route, handle) {
   function onRequest(request, response){
+    var postData = "";
     var pathname = url.parse(request.url).pathname;
     console.log("Request for " + pathname + " received.");
 
-    route(handle, pathname, response);
+    request.setEncoding("utf8");
+
+    request.addListener("data", function(postDataChunck){
+      postData += postDataChunck;
+      console.log("Received POST data chuck '"+ postDataChunck + "'.");
+    });
+
+    request.addListener("end", function(){
+      route(handle, pathname, response, postData);
+    });
   }
 
   http.createServer(onRequest).listen(8888);
